921d012a02bf1ddf60d1eb687a6b2594d7725185
Maploop <76199586+Maploop@users.noreply.github.com>
2023-09-18 15:13:51 +0330

    Add more to main page

--- mainpage.js
+++ mainpage.js
@@ -10,3 +10,14 @@ function responsiveNav()
       x.className = "topnav";
     }
   }
+
+  // window.onscroll = function() {scrollFunction()};
+
+  // function scrollFunction() 
+  // {
+  //   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) 
+  //   {
+  //     document.getElementById("navbar").style.top = "1000px";
+  //     console.log('pog');
+  //   }
+  // }
